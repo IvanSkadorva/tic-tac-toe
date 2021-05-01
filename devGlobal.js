a8e6137b46f64043a333ec43e16c0b47e348871e
Ivan Skadorva <vaniaskadorva@gmail.com>
2021-05-01 11:51:41 +0300

    Add pubHub integration

--- devGlobal.js
+++
@@ -1,9 +0,0 @@
-import {getGameRoom} from './src/state/gameRoom';
-
-const devGlobal = {};
-
-global.DEV_GLOBAL = devGlobal;
-
-export default devGlobal;
-
-global.DEV_GLOBAL.getGameRoom = getGameRoom;
--- src/config.js
+++
@@ -1,3 +0,0 @@
-const configDev = {SOCKET_SERVER_URL: 'http://localhost:3000'};
-const configProd = {};
-export const config = __DEV__ ? configDev : configProd;
